a5148267477cee8b1e710bccf0828b4bb8bd90c3
HongbiKo <rhghdql666@gmail.com>
2023-08-17 14:24:47 +0900

    popup function complete

--- src/js/popup.js
+++ src/js/popup.js
@@ -11,6 +11,8 @@ const popupSpecialRate = document.querySelector(".pop_specialrate");
 
 const closeBtn = document.querySelectorAll(".popup_close");
 
+// open popup
+
 btnRoom.addEventListener("click", function () {
   popupRoom.classList.add("on");
 });
@@ -21,22 +23,75 @@ btnSpecialRate.addEventListener("click", function () {
   popupSpecialRate.classList.add("on");
 });
 
+//close popup
+
 closeBtn.forEach((i) => {
   i.addEventListener("click", function () {
     this.parentNode.classList.remove("on");
   });
 });
 
+//
+//
 // change value
-const popupForm = document.querySelector(".popup_form");
+const popupForm = document.querySelectorAll(".popup_form");
 const roomNumber = document.querySelector(".popup_input.room");
 const roomBtnNum = document.querySelector(
   ".date_select_bar_option_btn_number_room"
 );
+const adultsNum = document.querySelector(".popup_input.adults");
+const kidsNum = document.querySelector(".popup_input.kids");
+const guestBtnNum = document.querySelector(
+  ".date_select_bar_option_btn_number_guest"
+);
 
-popupForm.addEventListener("submit", function (e) {
-  e.preventDefault();
+const specialRateNumber = document.querySelectorAll(".popup_input.checkbox");
+const specialRateBtnNum = document.querySelector(
+  ".date_select_bar_option_btn_number_specialrate"
+);
+
+popupForm.forEach((i) => {
+  i.addEventListener("submit", function (e) {
+    e.preventDefault();
+    if (this.name === "room") {
+      addRooms();
+    } else if (this.name === "guest") {
+      addGuests();
+    } else if (this.name === "specialrate") {
+      addSpecialRates();
+    }
+  });
+});
+
+function addRooms() {
   const roomNumberValue = roomNumber.value;
   roomBtnNum.textContent = roomNumberValue;
   popupRoom.classList.toggle("on");
-});
+}
+
+function addGuests() {
+  const adultsNumValue = adultsNum.value;
+  const kidsNumValue = kidsNum.value;
+
+  const guestNumber = parseInt(adultsNumValue) + parseInt(kidsNumValue);
+
+  guestBtnNum.textContent = guestNumber;
+
+  popupGuest.classList.toggle("on");
+}
+
+function addSpecialRates() {
+  let count = 0;
+  specialRateBtnNum.textContent = count;
+  specialRateNumber.forEach((i) => {
+    if (i.checked == true) {
+      count++;
+      specialRateBtnNum.textContent = count;
+      specialRateBtnNum.classList.add("on");
+    } else if (count === 0) {
+      specialRateBtnNum.classList.remove("on");
+    }
+  });
+
+  popupSpecialRate.classList.toggle("on");
+}
